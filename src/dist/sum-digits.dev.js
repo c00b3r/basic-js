2bf1c2e29398e8aa7b57f12a6cca2e5734ea8a89
c00b3r <sanuyOKmaklaren0>
2025-01-07 02:58:39 +0500

    feat: digit sum solution

--- src/dist/sum-digits.dev.js
+++ src/dist/sum-digits.dev.js
@@ -1,5 +1,7 @@
-const { NotImplementedError } = require('../extensions/index.js');
+"use strict";
 
+var _require = require('../extensions/index.js'),
+    NotImplementedError = _require.NotImplementedError;
 /**
  * Given a number, replace this number with
  * the sum of its digits until we get to a one digit number.
@@ -12,19 +14,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
+
+
 function getSumOfDigits(n) {
   if (n < 10) {
     return n;
   } else {
-    let sum = 0;
+    var sum = 0;
+
     while (n > 0) {
       sum += n % 10;
       n = Math.floor(n / 10);
     }
+
     return getSumOfDigits(sum);
   }
 }
 
 module.exports = {
-  getSumOfDigits,
+  getSumOfDigits: getSumOfDigits
 };
